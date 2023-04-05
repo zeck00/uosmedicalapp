657a1933f7efb3988c77f712522cf2e3cae1c6b1
zeck00 <a2005sdcardemom@gmail.com>
2023-04-05 23:05:30 +0400

    updates

--- lib/startup_page.dart
+++ lib/startup_page.dart
@@ -1,1 +1,20 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
+
+class StartupPage extends StatefulWidget {
+  const StartupPage({super.key});
+
+  @override
+  State<StartupPage> createState() => _StartupPageState();
+}
+
+class _StartupPageState extends State<StartupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: SlideAction()),
+    );
+  }
+}
