143474c2780dfec4bcae41c5e26d7ff7403605da
zeck00 <a2005sdcardemom@gmail.com>
2023-04-14 23:03:45 +0400

    new updates

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/screens/startup_page.dart';
+
+void main() {
+  runApp(MyApp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ),
+        home: StartupPage());
+  }
+}
---
+++ lib/services/flutter_local_notifications.dart
@@ -0,0 +1,8 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:timezone/timezone.dart';
+import 'package:timezone/data/latest.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotifServ {
+  LocalNotifServ();
+}
